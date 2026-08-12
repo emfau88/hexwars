c0275e97448a3470c2b82eb78a59c6e99e1209d9
emfau88 <voma@protonmail.ch>
2026-08-12 22:46:15 +0200

    Polish campaign menu typography

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import '@fontsource-variable/sora/wght.css';
+import '@fontsource-variable/source-sans-3/wght.css';
 import './styles.css';
 import './campaign-atlas.css';
 import { HexfrontApp } from './app/HexfrontApp';
